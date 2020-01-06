efc3b69e5e25226436dd74e01855ba05d0cbf439
seita_matsumoto <gyabooo@gmail.com>
2020-01-07 00:28:43 +0900

    ウィンドウサイズ判定追加

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 $(function () {
-  $('#menu-content a[href]').on('click', function (event) {
-    $('#menu-input').trigger('click');
-  });
+  $(window).resize(function () {
+    if (window.innerWidth < 812) {
+      $('#menu-content a[href]').on('click', function (event) {
+        $('#menu-input').trigger('click');
+      });
+    }
+  })
 })
